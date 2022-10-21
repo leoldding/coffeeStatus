9358d085484cb4a8bf967829a6a140af6e61a105
leoldding <leo.ld.ding@gmail.com>
2022-10-21 11:11:52 -0400

    solo container

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,8 @@
-yes = document.getElementById("yes");
-enroute = document.getElementById("enroute");
-no = document.getElementById("no");
+yes = document.getElementById("yes-container");
+enroute = document.getElementById("en-route-container");
+no = document.getElementById("no-container");
+solo = document.getElementById("solo-container");
+soloText = document.getElementById("solo-text");
 
 window.onload = function() {
     fetch("backend/loadstatus", {
@@ -11,6 +13,12 @@ window.onload = function() {
             if (status == "yes") {
                 yes.style.backgroundColor = "#4cff4c";
                 for (const child of yes.children) {
+                    child.style.color = "black";
+                }
+
+                solo.style.backgroundColor = "#4cff4c";
+                soloText.textContent = "Yes";
+                for (const child of solo.children) {
                     child.style.color = "black";
                 }
             } else {
@@ -25,6 +33,12 @@ window.onload = function() {
                 for (const child of enroute.children) {
                     child.style.color = "black";
                 }
+
+                solo.style.backgroundColor = "#ffff56";
+                soloText.textContent = "En Route";
+                for (const child of solo.children) {
+                    child.style.color = "black";
+                }
             } else {
                 enroute.style.backgroundColor = "gray";
                 for (const child of enroute.children) {
@@ -35,6 +49,12 @@ window.onload = function() {
             if (status == "no") {
                 no.style.backgroundColor = "#ff3434";
                 for (const child of no.children) {
+                    child.style.color = "black";
+                }
+
+                solo.style.backgroundColor = "#ff3434";
+                soloText.textContent = "No";
+                for (const child of solo.children) {
                     child.style.color = "black";
                 }
             } else {
